5330ae58ced24114862cd8575f2ca0c16a6cd5ea
unknown <barann.karabuluttt@gmail.com>
2026-04-06 14:51:31 +0300

    Deneme 2

--- app/page.tsx
+++ app/page.tsx
@@ -71,33 +71,33 @@ function Marquee() {
   const quadrupled = [...marqueeLogos, ...marqueeLogos, ...marqueeLogos, ...marqueeLogos];
   return (
     <div className="relative m-0 overflow-hidden bg-[#bfd730] transition-all duration-300
-      /* Yeşil Barın Yüksekliği: Mobilde daha tok durması için 45px'e çıkarıldı */
+      /* Yeşil Barın Yüksekliği */
       py-[45px] md:py-[55px] xl:py-[70px] 2xl:py-[90px]
       
-      /* Sol Gölgelendirme: Mobilde logoyu erken yutmaması için 40px'e düşürüldü */
+      /* Sol Gölgelendirme: Mobilde logoyu erkenden yutmasın diye 20px'e düşürüldü */
       before:pointer-events-none before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:bg-gradient-to-r before:from-[#bfd730] before:to-transparent
-      before:w-[40px] md:before:w-[100px] xl:before:w-[200px] 2xl:before:w-[300px]
+      before:w-[20px] md:before:w-[100px] xl:before:w-[200px] 2xl:before:w-[300px]
       
-      /* Sağ Gölgelendirme: Mobilde 40px'e düşürüldü */
+      /* Sağ Gölgelendirme: Mobilde 20px'e düşürüldü */
       after:pointer-events-none after:absolute after:right-0 after:top-0 after:z-10 after:h-full after:bg-gradient-to-l after:from-[#bfd730] after:to-transparent
-      after:w-[40px] md:after:w-[100px] xl:after:w-[200px] 2xl:after:w-[300px]
+      after:w-[20px] md:after:w-[100px] xl:after:w-[200px] 2xl:after:w-[300px]
     ">
       <div className="flex w-max items-center animate-[marquee_50s_linear_infinite]">
         {quadrupled.map((logo, i) => (
           <div key={i} className="shrink-0">
-            <div className="flex items-center justify-center opacity-100 brightness-0 invert transition-all duration-300 ease-in hover:scale-[1.15]
-              /* Logoların Arasındaki Boşluk ve Kutu Boyutları (Mobilde ciddi şekilde büyütüldü) */
-              mx-[45px] md:mx-[70px] xl:mx-[100px] 2xl:mx-[140px]
-              h-[32px] md:h-[40px] xl:h-[50px] 2xl:h-[65px]
-              w-[110px] md:w-[140px] xl:w-[180px] 2xl:w-[240px]
+            <div className="flex items-center justify-center opacity-100 brightness-0 invert transition-all duration-300 ease-in hover:scale-[1.05]
+              /* KAPSAYICI BOYUTLARI (Mobilde genişlik 160px'e, yükseklik 45px'e çıkarıldı) */
+              mx-[35px] md:mx-[70px] xl:mx-[100px] 2xl:mx-[140px]
+              h-[45px] md:h-[50px] xl:h-[60px] 2xl:h-[70px]
+              w-[160px] md:w-[180px] xl:w-[200px] 2xl:w-[260px]
             ">
               <Image 
                 src={logo.src} 
                 alt={logo.alt} 
                 width={300} 
                 height={120} 
-                /* SVG içindeki boşlukları tolere etmek için scale değerleri dengelendi */
-                className="w-full h-full object-contain scale-[1.3] md:scale-[1.4] 2xl:scale-[1.5]" 
+                /* Scale hilesi kaldırıldı, obje doğrudan büyük kapsayıcının içine tam yayıldı */
+                className="w-full h-full object-contain" 
               />
             </div>
           </div>
